c243fb5705b1a4ec86af072d4a4c38e82d69bedd
vsogawa <36315323+vsogawa@users.noreply.github.com>
2018-05-06 12:15:38 -0700

    Add files via upload

--- js/sw.js
+++ js/sw.js
@@ -1,24 +1,23 @@
-
-
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open('restaurant-static').then(function (cache) {
             return cache.addAll([
-                '/',
-                'js/main.js',
-                'js/restaurant_info.js',
-                'js/dbhelper.js',
-                'css/styles.css',
-                'img/1.jpg',
-                'img/2.jpg',
-                'img/3.jpg',
-                'img/4.jpg',
-                'img/5.jpg',
-                'img/6.jpg',
-                'img/7.jpg',
-                'img/8.jpg',
-                'img/9.jpg',
-                'img/10.jpg'
+                'main.js',
+                'restaurant_info.js',
+                'dbhelper.js',
+                '../css/styles.css',
+                '../index.html',
+                '../restaurant.html',
+                '../img/1.jpg',
+                '../img/2.jpg',
+                '../img/3.jpg',
+                '../img/4.jpg',
+                '../img/5.jpg',
+                '../img/6.jpg',
+                '../img/7.jpg',
+                '../img/8.jpg',
+                '../img/9.jpg',
+                '../img/10.jpg'
             ]);
         })
     );
